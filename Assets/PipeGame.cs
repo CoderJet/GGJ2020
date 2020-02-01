274da7c9e7dba05202b76b6e28f842ddb72e405d
Kakutasu <kakutasu@hotmail.com>
2020-02-01 18:16:58 +0000

    Pipe Minigame
    Added files for the pipe minigame.

---
+++ Assets/PipeGame.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PipeGame : MonoBehaviour
+{
+    public PipeTile pipeTile;
+    public int rowTilesCount = 5;
+    public int columnTilesCount = 5;
+
+    private GridLayoutGroup group;
+    public Sprite startSprite, endSprite, straightSprite, cornerSprite;
+    public Sprite startSpriteActive, endSpriteActive, straightSpriteActive, cornerSpriteActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        group = GetComponent<GridLayoutGroup>();
+        group.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        group.constraintCount = columnTilesCount;
+
+        for (int x = 0; x < columnTilesCount; x++)
+        {
+            for (int y = 0; y < rowTilesCount; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    PipeTile go = Instantiate(pipeTile, this.transform);
+                    go.currentDirection = circuits.Direction.right;
+                    go.PipeActive = true;
+                    go.type = circuits.TileType.start;
+                }
+                else if (x == columnTilesCount - 1 && y == rowTilesCount - 1)
+                {
+                    PipeTile go = Instantiate(pipeTile, this.transform);
+                    go.currentDirection = circuits.Direction.right;
+                    go.PipeActive = false;
+                    go.type = circuits.TileType.end;
+                }
+                else
+                {
+                    PipeTile go = Instantiate(pipeTile, this.transform);
+                    go.currentDirection = ((circuits.Direction)Random.Range(0, 4));
+                    go.type = ((circuits.TileType)Random.Range(2, 4));
+
+                    switch (go.type)
+                    {
+                        case circuits.TileType.corner:
+                            go.typeSprite = cornerSprite;
+                            go.typeSpriteActive = cornerSpriteActive;
+                            go.PipeActive = false;
+                            break;
+                        case circuits.TileType.straight:
+                            go.typeSprite = straightSprite;
+                            go.typeSpriteActive = straightSpriteActive;
+                            go.PipeActive = false;
+                            break;
+                        case circuits.TileType.end:
+                            go.typeSprite = endSprite;
+                            go.typeSpriteActive = endSpriteActive;
+                            go.PipeActive = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

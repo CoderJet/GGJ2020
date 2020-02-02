10a4255a9b4fec2e2d7dab692cd6e89d1aad2bd1
Admiral-Joshua <admiraljrn@outlook.com>
2020-02-02 12:51:50 +0000

    Addition of Robot Body sprites. Added some basic animation clips for some of the robot body parts.

---
+++ Assets/RobotHead01.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class RobotHead01 : MonoBehaviour
+{
+
+    int currentMood = 0;
+    string[] moodOptions = new string[]
+    {
+     "Happy", "Indifferent", "Angry", "Error",
+    };
+
+    currentMood = EditorGUILayout.Popup("Mood", currentMood, moodOptions); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
